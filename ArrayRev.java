31e7fd0c2728108a1ad993b76011320d8f6bb57f
Sumit <sumitsunet@gmail.com>
2024-06-08 13:10:35 +0530

    changes

--- ArrayRev.java
+++ ArrayRev.java
@@ -1,22 +1,37 @@
+import java.util.*;
 public class ArrayRev{
     public static void reverse(int number[]){
-
         int first = 0, last = number.length-1;
-        while (first<last) {
+
+        while(first<last){
             int temp = number[last];
             number[last] = number[first];
             number[first] = temp;
             first++;
             last--;
         }
+
     }
     public static void main(String[] args) {
-        int  number[] = {60,9,74,25,23,14,15};
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter N ");
+       int n = sc.nextInt();
+       int arr[] = new int[n];
+       System.out.println("Enter the array ");
+       for(int i=0; i<arr.length; i++){
+        arr[i] = sc.nextInt();
+       }
 
-        reverse(number);
-        for(int i=0; i<number.length; i++){
-            System.out.print(number[i] + " "); 
-        }
-        System.out.println();
+       System.out.print("Enterted array is ");
+       for(int i=0; i<arr.length; i++){
+        System.out.print(arr[i] +" ");
+       }
+
+       reverse(arr);
+       System.out.print("Reversed array is ");
+       for(int i=0; i<arr.length; i++){
+        System.out.print(arr[i] +" ");
+       }
+       sc.close();
     }
 }
