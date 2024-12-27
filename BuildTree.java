505e8a5c41874860ac8cad9768b96d3bc8fc5888
Sumit <sumitsunet@gmail.com>
2024-12-27 13:24:56 +0530

    changes

--- BuildTree.java
+++ BuildTree.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class BuildTree {
     static class Node{
         int data;
@@ -11,6 +15,8 @@ public class BuildTree {
     }
 
     static class BTree{
+
+        //Building a Tree
         static int idx = -1;
         public static Node buildTree(int nodes[]){
             idx++;
@@ -24,7 +30,7 @@ public class BuildTree {
             return newNode;
         }
         
-        // Pre Order
+        //Pre Order
         public static void preOrder(Node root){
             if(root == null){
                 return;
@@ -56,6 +62,75 @@ public class BuildTree {
         }
 
 
+        //Level Order Traversal
+        public static void LevelOrder(Node root){
+            if(root ==  null){
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while(!q.isEmpty()){
+                Node currNode = q.remove();
+                if(currNode == null){
+                    System.out.println();
+                    if(q.isEmpty()) break;
+                    else q.add(null);
+                }
+                else{
+                    System.out.print(currNode.data+ " ");
+                    if(currNode.left != null){
+                        q.add((currNode.left));
+                    }
+                    if(currNode.right != null){
+                        q.add(currNode.right);
+                    }
+                }
+            }
+        }
+
+        //Height of Tree
+        public static int heightT(Node root){
+            if(root == null){
+                return 0;
+            }
+            
+            int lh = heightT(root.left);
+            int rh = heightT(root.right);
+            return Math.max(lh, rh) + 1;     
+        }
+
+        //Count Of Nodes
+        public static int nodeCount(Node root){
+            if(root == null){
+                return 0;
+            }
+            int lcount = nodeCount(root.left);
+            int rcount = nodeCount(root.right);
+            return lcount + rcount + 1;
+        }
+
+        //Sum of Nodes
+        public static int sum(Node root){
+            if(root == null) return 0;
+            int lsum = sum(root.left);
+            int rsum = sum(root.right);
+            return lsum + rsum + root.data;
+        }
+
+        //Largest element in ecah row
+
+        public static void largestValue(Node root){
+            if(root == null){
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            
+            
+        }
+
+
     }
 
 
@@ -70,7 +145,12 @@ public class BuildTree {
         tree.inorder(root);
         System.out.println("Post Order Traversal");
         tree.postOrder(root);
-
+        System.out.println("Level Order Traversla");
+        tree.LevelOrder(root);
+        System.out.println();
+        System.out.println("Height of Tree : " + tree.heightT(root));
+        System.out.println("Count of Node " + tree.nodeCount(root));
+        System.out.println("Sum of nodes " + tree.sum(root));
     }
     
 }
